ec3b9a4e9aaf18b491414f6a1d3aff4e3d6337c3
DJ Taylor <dtaylor88@gmail.com>
2021-09-02 16:15:18 -0700

    commit tree 9d751d628472d44c93851ccc4a5214a7fb7c2af8

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+class Polygon {
+    constructor(integers) {
+        this.sides = integers;
+    }
+
+    get countSides() {
+        return this.sides.length;
+    }
+
+    get perimeter() {
+        return this.sides.reduce((a, b) => a + b, 0)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        let a = this.sides[0];
+        let b = this.sides[1];
+        let c = this.sides[2];
+
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+       return this.sides.every((val, i) => val === this.sides[0])
+    }
+
+    get area() {
+        if (this.isValid) {
+          return this.sides[0] ** 2
+        } else {
+            throw new Error('Sorry, this is not a valid square')
+        }
+    }
+}
